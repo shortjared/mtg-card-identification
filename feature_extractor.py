7c01eee817efb02f7ec6197976a55dbcbb74e97f
Jared Short <jaredlshort@gmail.com>
2021-05-28 02:34:19 +0000

    feat: new ideas

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,5 +1,6 @@
+from PIL import Image, ImageOps
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.applications.efficientnet import EfficientNetB7, preprocess_input
 from tensorflow.keras.models import Model
 import numpy as np
 
@@ -7,8 +8,8 @@ import numpy as np
 
 class FeatureExtractor:
     def __init__(self):
-        base_model = VGG16(weights='imagenet')
-        self.model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
+        base_model = EfficientNetB7(weights='imagenet')
+        self.model = Model(inputs=base_model.input, outputs=base_model.output)
 
     def extract(self, img):
         """
@@ -19,9 +20,25 @@ class FeatureExtractor:
         Returns:
             feature (np.ndarray): deep feature with the shape=(4096, )
         """
-        img = img.resize((224, 224))  # VGG must take a 224x224 img as an input
-        img = img.convert('RGB')  # Make sure img is color
-        x = image.img_to_array(img)  # To np.array. Height x Width x Channel. dtype=float32
+        
+        desired_size = 600
+        old_size = img.size
+        ratio = float(desired_size)/max(old_size)
+        new_size = tuple([int(x*ratio) for x in old_size])
+
+        img = img.resize(new_size, Image.ANTIALIAS)
+        # create a new image and paste the resized on it
+
+        new_im = Image.new("RGB", (desired_size, desired_size))
+        new_im.paste(img, ((desired_size-new_size[0])//2,
+                            (desired_size-new_size[1])//2))
+                            
+        delta_w = desired_size - new_size[0]
+        delta_h = desired_size - new_size[1]
+        padding = (delta_w//2, delta_h//2, delta_w-(delta_w//2), delta_h-(delta_h//2))
+        padded_img = ImageOps.expand(img, padding)
+        tf_ready_img = padded_img.convert('RGB')  # Make sure img is color
+        x = image.img_to_array(tf_ready_img)  # To np.array. Height x Width x Channel. dtype=float32
         x = np.expand_dims(x, axis=0)  # (H, W, C)->(1, H, W, C), where the first elem is the number of img
         x = preprocess_input(x)  # Subtracting avg values for each pixel
         feature = self.model.predict(x)[0]  # (1, 4096) -> (4096, )
